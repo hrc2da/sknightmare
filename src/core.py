3c3fba9bbcd281aa6cf8c7876f3c60d3d2ffc71a
mattvlaw <mattvlaw@gmail.com>
2019-03-10 17:10:55 -0400

    added a main and added numpy and scipy depends

--- src/core.py
+++ src/core.py
@@ -327,3 +327,21 @@ class Party:
       check = subtotal + tip
       yield seating.put(self.table)
     return check, self.satisfaction
+
+
+if __name__=="__main__":
+    equipment = [{"name":"Lame Pizza Oven", 
+              "attributes":{"capabilities":["oven","pizza","steak"],
+                                          "cook_time_mean":10, "cook_time_std":1, 
+                                          "quality_mean":0.5, "quality_std":0.4}},
+            
+                {"name":"Awesome Pizza Oven", 
+                "attributes":{"capabilities":["oven","pizza","steak"],
+                                            "cook_time_mean":4, "cook_time_std":0.1, 
+                                            "quality_mean":0.7, "quality_std":0.1}}]
+    tables = [{"name":"Table 1", 
+            "attributes":{"x":2.1,"y":3.7,"radius":4,"seats":2}},
+            {"name":"Table 2", 
+            "attributes":{"x":4.1,"y":5.7,"radius":7,"seats":5}}]
+    r = Restauraunt("Sophie's Kitchen", [equipment[1]], tables)
+    r.simulate(days=1)
