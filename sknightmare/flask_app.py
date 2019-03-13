d427b3ed45770286d5a28263820736dfcee222b4
Amrit <ak2244@cornell.edu>
2019-03-13 16:33:41 -0400

    started scoket refactor for flask

--- sknightmare/flask_app.py
+++ sknightmare/flask_app.py
@@ -1,16 +1,31 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_socketio import SocketIO, send, emit
 from restaurant import Restaurant
 app = Flask(__name__)
 CORS(app)
+socketio = SocketIO(app)
+
+
 @app.route('/simulate', methods=['POST'])
 def simulate():
     layout = request.get_json(force=True)
     print(layout)
     r = Restaurant("Sophie's Kitchen", layout["equipment"], layout["tables"])
     r.simulate(days=int(layout["days"]))
-    return jsonify({"report":r.final_report()})
-    
+    return jsonify({"report": r.final_report()})
+
+
+@socketio.on('connect')
+def handle_connect():
+    sess = str(time.time())
+    emit('session_id', sess)
+
+
+@socketio.on('simulate')
+def socket_simulate(restaurant):
+    pass
+
 
 if __name__ == "__main__":
-    app.run()
+    socketio.run(app, host='0.0.0.0')
