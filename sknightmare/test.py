70bdab9d5982ccd22cef40d68978273729c4f4f5
mattvlaw <mattvlaw@gmail.com>
2019-03-19 19:27:26 -0400

    trying to get test to pass

--- sknightmare/test.py
+++ sknightmare/test.py
@@ -1,20 +1,29 @@
+import unittest
 from restaurant import Restaurant
 
-equipment = [{"name": "Lame Pizza Oven",
-              "attributes": {"capabilities": ["oven", "pizza", "steak"],
-                             "cook_time_mean":10, "cook_time_std":1,
-                             "quality_mean":0.5, "quality_std":0.4}},
 
-             {"name": "Awesome Pizza Oven",
-              "attributes": {"capabilities": ["oven", "pizza", "steak"],
-                             "cook_time_mean":4, "cook_time_std":0.1,
-                             "quality_mean":0.7, "quality_std":0.1}}]
-tables = [{"name": "Table 1",
-           "attributes": {"x": 0.1, "y": 0.7, "radius": 4, "seats": 2}},
-          {"name": "Table 2",
-           "attributes": {"x": 0.4, "y": 0.2, "radius": 7, "seats": 5}}]
+class HelloTest(unittest.TestCase):
+    def test_the_test(self):
+        self.assertEqual(1+1, 2)
 
-staff = [{'x': .5, 'y': 0.5}]
 
-r = Restauraunt("Sophie's Kitchen", equipment, tables, staff)
-r.simulate(days=1)
+if __name__ == '__main__':
+    unittest.main()
+# equipment = [{"name": "Lame Pizza Oven",
+#               "attributes": {"capabilities": ["oven", "pizza", "steak"],
+#                              "cook_time_mean":10, "cook_time_std":1,
+#                              "quality_mean":0.5, "quality_std":0.4}},
+
+#              {"name": "Awesome Pizza Oven",
+#               "attributes": {"capabilities": ["oven", "pizza", "steak"],
+#                              "cook_time_mean":4, "cook_time_std":0.1,
+#                              "quality_mean":0.7, "quality_std":0.1}}]
+# tables = [{"name": "Table 1",
+#            "attributes": {"x": 0.1, "y": 0.7, "radius": 4, "seats": 2}},
+#           {"name": "Table 2",
+#            "attributes": {"x": 0.4, "y": 0.2, "radius": 7, "seats": 5}}]
+
+# staff = [{'x': .5, 'y': 0.5}]
+
+# r = Restauraunt("Sophie's Kitchen", equipment, tables, staff)
+# r.simulate(days=1)
