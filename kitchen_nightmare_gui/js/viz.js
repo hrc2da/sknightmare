7d1504b8e836a42af068175b85089d91ff49e0c0
Amrit <ak2244@cornell.edu>
2019-03-19 11:05:33 -0400

    added clearing and updating min values

--- kitchen_nightmare_gui/js/viz.js
+++ kitchen_nightmare_gui/js/viz.js
@@ -2,10 +2,9 @@ class TrackingGraph {
     constructor(svg, x, y, w, h, attribute, id, name = undefined) {
         this.svg = svg;
         this.attribute = attribute;
-        if(name){
+        if (name) {
             this.name = name;
-        }
-        else{
+        } else {
             this.name = attribute;
         }
         this.num_days = 0;
@@ -55,30 +54,30 @@ class TrackingGraph {
             .scale(attribute_scale)
             .ticks(2);
         svg.append("text")
-            .attr("x",this.x + this.width/2)
-            .attr("y",this.y + this.padding)
+            .attr("x", this.x + this.width / 2)
+            .attr("y", this.y + this.padding)
             .attr("fill", "black")
             .text(this.name);
 
         svg.append("g")
             .attr("class", "x_axis")
-            .attr("id", "x_axis_"+this.id)
-            .attr("transform", "translate(0," + ( this.height + this.y ) + ")")
+            .attr("id", "x_axis_" + this.id)
+            .attr("transform", "translate(0," + (this.height + this.y) + ")")
             .call(xAxis);
 
         // Add the Y Axis
         svg.append("g")
             .attr("class", "y_axis")
-            .attr("id", "y_axis_"+this.id)
+            .attr("id", "y_axis_" + this.id)
             .attr("transform", "translate(" + this.padding + ",0)")
             .call(yAxis);
 
-        svg.selectAll(".data_points_"+this.id)
+        let point_selection = svg.selectAll(".data_points_" + this.id)
             .data(this.data)
             .enter()
             .append("circle")
             .attrs({
-                class: "data_points_"+this.id,
+                class: "data_points_" + this.id,
                 cx: d => {
                     return this.make_x_scale()(d.day);
                 },
@@ -88,12 +87,15 @@ class TrackingGraph {
                 r: 2,
                 fill: "#07103A"
             });
+        point_selection.exit().remove();
 
-        svg.append("path")
+        let path_selection = svg.append("path")
             .data([this.data])
-            .attr("id", "graph_path_"+this.id)
+            .attr("id", "graph_path_" + this.id)
             .attr("class", "graph_path")
             .attr("d", this.line_generator(this.make_x_scale(), this.make_y_scale(), this.attribute));
+
+        path_selection.exit().remove()
     };
 
     addDayReport = report => {
@@ -102,7 +104,18 @@ class TrackingGraph {
         if (report[this.attribute] > this.max_val) {
             this.max_val = report[this.attribute];
         }
+        if (report[this.attribute] < this.min_val) {
+            this.min_val = report[this.attribute];
+        }
     };
+
+    clear = () => {
+        this.data = [];
+        this.num_days = 0;
+        this.max_val = 1;
+        this.min_val = 0;
+        this.initialize();
+    }
 
     update = report => {
         this.num_days++;
@@ -119,42 +132,42 @@ class TrackingGraph {
             .axisLeft()
             .scale(attribute_scale)
             .ticks(2);
-        this.svg.selectAll("#x_axis_"+this.id).remove();
-        this.svg.selectAll("#y_axis_"+this.id).remove();
+        this.svg.selectAll("#x_axis_" + this.id).remove();
+        this.svg.selectAll("#y_axis_" + this.id).remove();
 
         this.svg
             .append("g")
             .attr("class", "x_axis")
-            .attr("id", "x_axis_"+this.id)
-            .attr("transform", "translate(0," + (this.y + this.height) + ")")
+            .attr("id", "x_axis_" + this.id)
+            .attr("transform", "translate(0," + (attribute_scale(0) + ")"))
             .call(xAxis);
 
         // Add the Y Axis
         this.svg
             .append("g")
             .attr("class", "y_axis")
-            .attr("id", "y_axis_"+this.id)
+            .attr("id", "y_axis_" + this.id)
             .attr("transform", "translate(" + this.padding + ",0)")
             .call(yAxis);
 
-        let point_selection = this.svg.selectAll(".data_points_"+this.id);
+        let point_selection = this.svg.selectAll(".data_points_" + this.id);
         point_selection
             .data(this.data)
             .transition()
             .duration(400)
             .attrs({
-                class: "data_points_"+this.id,
+                class: "data_points_" + this.id,
                 cx: d => {
                     return day_scale(d.day);
                 },
                 cy: d => {
                     return attribute_scale(d[this.attribute]);
                 },
-                r: 2,
+                r: 1,
                 fill: "#07103A"
             });
 
-        let path_selection = this.svg.selectAll("#graph_path_"+this.id);
+        let path_selection = this.svg.selectAll("#graph_path_" + this.id);
         path_selection
             .data([this.data])
             .transition()
@@ -162,85 +175,107 @@ class TrackingGraph {
             .attrs({
                 d: this.line_generator(day_scale, attribute_scale, this.attribute)
             });
+        path_selection.exit().remove();
 
         point_selection
             .data(this.data)
             .enter()
             .append("circle")
             .attrs({
-                class: "data_points_"+this.id,
+                class: "data_points_" + this.id,
                 cx: d => {
                     return day_scale(d.day);
                 },
                 cy: d => {
                     return attribute_scale(d[this.attribute]);
                 },
-                r: 2,
+                r: 1,
                 fill: "#07103A"
             });
+        point_selection.exit().remove();
     };
 }
 
 class Viz {
     constructor(svg, x, y, w, h) {
-        let ph = h-2*45;
+        this.svg = svg;
+        let ph = h - 2 * 45;
         //ph = h;
         this.num_metrics = 4;
         this.plots = {}
-        this.plots["entries_graph"] = new TrackingGraph(svg, x, y, w, ph / this.num_metrics, "entries", 0, name="customers");
+        this.plots["entries_graph"] = new TrackingGraph(svg, x, y, w, ph / this.num_metrics, "entries", 0, name = "customers");
         this.plots["revenue_graph"] = new TrackingGraph(svg, x, y + ph / this.num_metrics, w, ph / this.num_metrics, "revenue", 1);
-        this.plots["profit_graph"] = new TrackingGraph(svg, x, y + 2*ph / this.num_metrics, w, ph / this.num_metrics, "profit", 2);
-        this.plots["satisfaction_graph"] = new TrackingGraph(svg, x, y + 3*ph / this.num_metrics, w, ph / this.num_metrics, "satisfaction", 3, name="customer satisfaction");
+        this.plots["profit_graph"] = new TrackingGraph(svg, x, y + 2 * ph / this.num_metrics, w, ph / this.num_metrics, "profit", 2);
+        this.plots["satisfaction_graph"] = new TrackingGraph(svg, x, y + 3 * ph / this.num_metrics, w, ph / this.num_metrics, "satisfaction", 3, name = "customer satisfaction");
     }
 
     initialize() {
-        for(let metric in this.plots){
+        for (let metric in this.plots) {
             this.plots[metric].initialize();
         }
 
     }
+
+    clear() {
+        this.svg.selectAll("*").remove(); // sorry
+        for (let metric in this.plots) {
+            this.plots[metric].clear();
+        }
+    }
     update(report) {
-        for(let metric in this.plots){
+        for (let metric in this.plots) {
             this.plots[metric].update(report);
         }
- 
+
 
     }
 }
 
-function generateRating(title,rating,icon){
-    console.log(title,rating);
+function generateRating(title, rating, icon) {
+    console.log(title, rating);
     let stringBuilder = "<p>";
     stringBuilder += title + ": ";
-    for(let i=0; i<rating; i++){
-        stringBuilder += "<i class='material-icons'>"+icon+"</i>";
+    for (let i = 0; i < rating; i++) {
+        stringBuilder += "<i class='material-icons'>" + icon + "</i>";
     }
     stringBuilder += "</p>";
     return stringBuilder;
 }
 
-function showReport(report){
+function showReport(report) {
     $('#report').empty();
     $('#ratings').empty();
     $('#finances').empty();
     $('#report').append("<h2>Restaurant Simulation Summary</h2>");
     $('#ratings').append("<h3>Ratings and Reviews</h3>");
-    $('#ratings').append("<p>Number of Days Simulated: "+report["num_days"]+"</p>");
-    $('#ratings').append(generateRating("Overall Rating",Math.round(report["satisfaction"]*7),"star"));    
-    $('#ratings').append(generateRating("Price Range",Math.round(report["avg_normalized_check"]*7),"attach_money"));
-    $('#ratings').append(generateRating("Service Rating",Math.round(report["service_rating"]*5),"sentiment_satisfied"));
-    $('#ratings').append(generateRating("Busyness",Math.ceil(report["daily_customers"]/50),"person"));
-    $('#ratings').append(generateRating("Noisiness",Math.round(report["avg_noise"]/4),"hearing"));
-    $('#ratings').append("<p>Average Wait Time: "+report["wait_times"][0].toLocaleString(undefined, {maximumFractionDigits:2})+"</p>");
-    $('#ratings').append("<p>Average Group Size: "+report["avg_party_size"].toLocaleString(undefined, {maximumFractionDigits:2})+"</p>");
+    $('#ratings').append("<p>Number of Days Simulated: " + report["num_days"] + "</p>");
+    $('#ratings').append(generateRating("Overall Rating", Math.round(report["satisfaction"] * 7), "star"));
+    $('#ratings').append(generateRating("Price Range", Math.round(report["avg_normalized_check"] * 7), "attach_money"));
+    $('#ratings').append(generateRating("Service Rating", Math.round(report["service_rating"] * 5), "sentiment_satisfied"));
+    $('#ratings').append(generateRating("Busyness", Math.ceil(report["daily_customers"] / 50), "person"));
+    $('#ratings').append(generateRating("Noisiness", Math.round(report["avg_noise"] / 4), "hearing"));
+    $('#ratings').append("<p>Average Wait Time: " + report["wait_times"][0].toLocaleString(undefined, {
+        maximumFractionDigits: 2
+    }) + "</p>");
+    $('#ratings').append("<p>Average Group Size: " + report["avg_party_size"].toLocaleString(undefined, {
+        maximumFractionDigits: 2
+    }) + "</p>");
     $('#finances').append("<h3>Finances</h3>");
-    $('#finances').append("<p>Total Revenue: <span class='revenue'>$"+report["revenue"].toLocaleString(undefined, {maximumFractionDigits:2})+"</span></p>");
-    $('#finances').append("<p>Upfront Costs: <span class='expenses'>$"+report["upfront_costs"].toLocaleString(undefined, {maximumFractionDigits:2})+"</span></p>");
-    $('#finances').append("<p>Staffing, Food, and Upkeep Costs: <span class='expenses'>$"+report["total_overhead"].toLocaleString(undefined, {maximumFractionDigits:2})+"</span></p>");
+    $('#finances').append("<p>Total Revenue: <span class='revenue'>$" + report["revenue"].toLocaleString(undefined, {
+        maximumFractionDigits: 2
+    }) + "</span></p>");
+    $('#finances').append("<p>Upfront Costs: <span class='expenses'>$" + report["upfront_costs"].toLocaleString(undefined, {
+        maximumFractionDigits: 2
+    }) + "</span></p>");
+    $('#finances').append("<p>Staffing, Food, and Upkeep Costs: <span class='expenses'>$" + report["total_overhead"].toLocaleString(undefined, {
+        maximumFractionDigits: 2
+    }) + "</span></p>");
     let profitability = "expenses";
-    if(report["profit"] > 0){
+    if (report["profit"] > 0) {
         profitability = "revenue";
     }
-    $('#finances').append("<p>Profit: <span class='"+profitability+"'>$"+report["profit"].toLocaleString(undefined, {maximumFractionDigits:2})+"</span></p>");
+    $('#finances').append("<p>Profit: <span class='" + profitability + "'>$" + report["profit"].toLocaleString(undefined, {
+        maximumFractionDigits: 2
+    }) + "</span></p>");
 
 }
